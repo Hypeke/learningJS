75a12685e683a4ecab033baaccfb7929bf2dbc00
Nurasil <nurasyl1999j@gmail.com>
2024-06-08 11:45:40 +0600

    первы урок создание оценки просмотренных фильмов

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log("Dermo");
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?","");
+
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+}
+
+let film = prompt("Один из последних просмотренных фильмов?", "");
+let ocenka = prompt('На сколько оцените его?');
+let film2 = prompt("Один из последних просмотренных фильмов?", "");
+let ocenka2 = prompt('На сколько оцените его?');
+
+personalMovieDB.movies[film] = ocenka;
+personalMovieDB.movies[film2] = ocenka2;
+
+console.log(personalMovieDB);
